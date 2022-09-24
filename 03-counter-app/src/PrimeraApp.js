3b9e86dc63d0bd17df2506d212c21c85b10e812d
EndYou <jorgesenjudo@gmail.com>
2022-09-24 10:24:58 +0200

    Modificaciones a 02, a 03 y agregando 04

---
+++ 03-counter-app/src/PrimeraApp.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import PropTypes from "prop-types";
+
+
+//FC
+
+const PrimerApp = ( { saludo, subtitulo } ) => {
+
+    
+
+    return (
+
+        <>
+            <h1> {saludo}</h1>
+            {/* <pre> { JSON.stringify(saludo, null, 3 ) } </pre> */}
+            <p>{subtitulo}</p>
+        </>
+        
+    )
+
+}
+
+PrimerApp.propTypes = {
+    saludo: PropTypes.string.isRequired 
+}
+
+PrimerApp.defaultProps = {
+    subtitulo: 'Soy un subtitulo'
+}
+
+
+export default PrimerApp
