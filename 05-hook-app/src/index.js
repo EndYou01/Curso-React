2591da0932dbe4e544e12bc26c0a1f9aad5bb361
EndYou <jorgesenjudo@gmail.com>
2022-10-10 15:54:07 -0400

    Avanzando en 05

--- 05-hook-app/src/index.js
+++ 05-hook-app/src/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 // import { HookApp } from './HookApp';
 // import {CounterApp} from './components/01-useState/CounterApp'
-import { CounterWithCustomHook } from './components/01-useState/CounterWithCustomHook';
+// import { CounterWithCustomHook } from './components/01-useState/CounterWithCustomHook';
+// import { SimpleForm } from './components/02-useEfect/SimpleForm';
+import { FormWithCustomHook } from './components/02-useEfect/FormWithCustomHook';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-    <CounterWithCustomHook />
+    <FormWithCustomHook />
 );
 
