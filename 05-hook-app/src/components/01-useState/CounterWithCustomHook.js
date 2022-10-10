61f1848ba3bd47366c7e1e3b3d89e6d75fd89346
EndYou <jorgesenjudo@gmail.com>
2022-10-10 13:22:53 -0400

    Avanzando en 05

---
+++ 05-hook-app/src/components/01-useState/CounterWithCustomHook.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import'./counter.css'
+
+export const CounterWithCustomHook = () => {
+
+    
+
+
+
+    return (
+        <>
+        Counter with hook: {0} 
+        <hr />
+        
+        <button className='btn'>+ 1</button>
+        <button className='btn'>- 1</button>
+        </>
+    )
+}
