36409469b760c8cf2ffacdacd532c54d669ff3ad
EndYou <jorgesenjudo@gmail.com>
2022-10-13 22:44:19 -0500

    Avanzando en 05

--- 05-hook-app/src/index.js
+++ 05-hook-app/src/index.js
@@ -4,11 +4,14 @@ import ReactDOM from 'react-dom/client';
 // import {CounterApp} from './components/01-useState/CounterApp'
 // import { CounterWithCustomHook } from './components/01-useState/CounterWithCustomHook';
 // import { SimpleForm } from './components/02-useEfect/SimpleForm';
-import { FormWithCustomHook } from './components/02-useEfect/FormWithCustomHook';
+// import { FormWithCustomHook } from './components/02-useEffect/FormWithCustomHook';
+// import { MultipleCustomHooks } from './components/03-examples/MultipleCustomHooks';
+// import { FocusScreen } from './components/04-useRef/FocusScreen';
+import { RealExampleRef } from './components/04-useRef/RealExampleRef';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-    <FormWithCustomHook />
+    <RealExampleRef />
 );
 
