09549a894a9cbe9729cff9818114c7fea1d42c37
EndYou <jorgesenjudo@gmail.com>
2022-11-06 12:53:38 -0500

    Concluyendo Fase de Pruebas

--- 05-hook-app/src/setupTests.js
+++ 05-hook-app/src/setupTests.js
@@ -1,7 +1,9 @@
-import Enzyme from 'enzyme';
+import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
+
 import {createSerializer} from 'enzyme-to-json';
 
-Enzyme.configure({ adapter: new Adapter() });
+configure({ adapter: new Adapter() });
+
 
 expect.addSnapshotSerializer(createSerializer({mode: 'deep'}));
