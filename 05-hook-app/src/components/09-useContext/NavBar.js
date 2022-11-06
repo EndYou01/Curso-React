e7ef532ac10625a1638fee19b9ea20d1f1760bd1
EndYou <jorgesenjudo@gmail.com>
2022-11-06 11:44:26 -0500

    creando archivo setupTests en 05/src

--- 05-hook-app/src/components/09-useContext/NavBar.js
+++ 05-hook-app/src/components/09-useContext/NavBar.js
@@ -1,15 +1,8 @@
 import React from 'react'
-import { Link, NavLink } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 
 export const NavBar = () => {
   return (
-    // <nav>
-    //     <ul>
-    //         <li><Link to="/">Home</Link></li>
-    //         <li><Link to="./about">About</Link></li>
-    //         <li><Link to="./login">Login</Link></li>
-    //     </ul>
-    // </nav>
     <nav className="navbar navbar-expand-sm bg-dark navbar-dark">
         <div className="container-fluid">
             <a className="navbar-brand" href="/">useContext</a>
