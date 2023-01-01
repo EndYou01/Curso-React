4c3128ba84fbb5f62aed760a16e5de52ea100f1d
EndYou <jorgesenjudo@gmail.com>
2023-01-01 15:49:06 -0500

    Iniciando con React-Redux

---
+++ 08-journal-app/src/reducers/authReducer.js
@@ -0,0 +1,29 @@
+
+
+/*
+    {
+        uid: dabbwkbdkw,
+        name: Senjudo
+    }
+*/
+
+import { types } from "../types/types";
+
+
+export const authReducer = (state = {}, action) =>{
+
+    switch(action.type){
+        case types.login:
+            return{
+                uid: action.payload.uid,
+                name: action.payload.displayName,
+            }
+        
+        case types.logout:
+            return{}
+
+        default:
+            return state
+    }
+
+}
