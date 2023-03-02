28956a28310969540bc4855c21f009aa36a00854
EndYou <jorgesenjudo@gmail.com>
2023-03-02 16:54:31 -0500

    Agregando Variables de entorno en JournalApp

--- 08-journal-app/src/firebase/firebase-config.js
+++ 08-journal-app/src/firebase/firebase-config.js
@@ -22,27 +22,27 @@ const firebaseConfig = {
   appId: "1:753627153296:web:33fd9287f5cb1a7bc9e915"
 };
 
-const firebaseConfigTesting = {
-  apiKey: "AIzaSyD5-4gUUrMLCzTWDEJ3QpkmfIboN5PDCq4",
-  authDomain: "push-one-signal-17ede.firebaseapp.com",
-  databaseURL: "https://push-one-signal-17ede.firebaseio.com",
-  projectId: "push-one-signal-17ede",
-  storageBucket: "push-one-signal-17ede.appspot.com",
-  messagingSenderId: "803724161810",
-  appId: "1:803724161810:web:02f32ebc98a71e376339cb"
-}
+// const firebaseConfigTesting = {
+//   apiKey: "AIzaSyD5-4gUUrMLCzTWDEJ3QpkmfIboN5PDCq4",
+//   authDomain: "push-one-signal-17ede.firebaseapp.com",
+//   databaseURL: "https://push-one-signal-17ede.firebaseio.com",
+//   projectId: "push-one-signal-17ede",
+//   storageBucket: "push-one-signal-17ede.appspot.com",
+//   messagingSenderId: "803724161810",
+//   appId: "1:803724161810:web:02f32ebc98a71e376339cb"
+// }
 
 
-if (process.env.NODE_ENV === 'test') {
-  // testing
-  firebase.initializeApp(firebaseConfigTesting);
-} else {
-  // dev/prod
-  firebase.initializeApp(firebaseConfig);
-}
+// if (process.env.NODE_ENV === 'test') {
+//   // testing
+//   firebase.initializeApp(firebaseConfigTesting);
+// } else {
+//   // dev/prod
+//   firebase.initializeApp(firebaseConfig);
+// }
 
 // Initialize Firebase
-// firebase.initializeApp(firebaseConfig);
+firebase.initializeApp(firebaseConfig);
 // // const app = initializeApp(firebaseConfig);
 
 const db = firebase.firestore();
